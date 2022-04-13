83d55d9f79233ffea9b397dc5d6da5364e9fd105
Mark Stuppacher <>
2022-04-13 15:26:13 +0200

    Add verified parameter

--- Casks/citrix-files.rb
+++ Casks/citrix-files.rb
@@ -2,7 +2,8 @@ cask "citrix-files" do
   version "22.2"
   sha256 "389a5c198e9a2153fd28b48412237b332e5abbb8d72bd87a074cf5b467bd8249"
 
-  url "https://dl.sharefile.com/cfmac/Citrix%20Files%20v#{version}%20(14rc1).dmg"
+  url "https://dl.sharefile.com/cfmac/Citrix%20Files%20v#{version}%20(14rc1).dmg",
+      verified: "dl.sharefile.com/cfmac/"
   name "Citrix Files"
   name "Citrix ShareFile"
   desc "ShareFile is a secure content collaboration, file sharing and sync software that supports all the document-centric tasks and workflow needs of small and large businesses."
